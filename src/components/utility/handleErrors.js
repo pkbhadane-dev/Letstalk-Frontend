eb1937f26e4ba7f46a14bc0a6d8f841cec900ade
Prashant Bhadane <pkbhadane545@gmail.com>
2026-05-12 21:27:08 +0530

    improve error handling

--- src/components/utility/handleErrors.js
+++ src/components/utility/handleErrors.js
@@ -1,6 +1,9 @@
 export const HandleError = (error, rejectWithValue) => {
   const errorMassege = error?.response?.data?.errors[0]?.msg; //validation error
   const customErrMessage = error?.response?.data?.errors; // custom error
+  const legacyCustomError = typeof error?.response?.data?.errors === 'string' 
+    ? error.response.data.errors 
+    : null;
   const networkError = error?.message;
-  return rejectWithValue(errorMassege || customErrMessage || networkError);
+  return rejectWithValue(errorMassege || customErrMessage || networkError || legacyCustomError);
 };
